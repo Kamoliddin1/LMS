6ff74f7d97419da88a92b0f02c3c707e32fa8eb9
sardor0990 <sardorzufarov98@gmail.com>
2019-12-13 11:04:35 +0300

    colloboration accepted

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,6 @@
-module LMSProject {
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens com.main;
-    opens com.login;
-}
+//module LMSProject {
+//    requires javafx.fxml;
+//    requires javafx.controls;
+//    opens com.main;
+//    opens com.login;
+//}
