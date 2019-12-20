4e2965f67545cb026a8f5d069305ff5d6fe02db7
backendAlgo <>
2019-12-20 23:30:26 +0500

    Singleton connection

--- src/com/main/SingletonCon.java
+++ src/com/main/SingletonCon.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Demo {
+public class SingletonCon {
     static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
     // jdbc Connection
     private static Connection conn = null;
